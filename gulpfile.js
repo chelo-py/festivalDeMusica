d5a406f27c250cb9e177f7fc9d7addd943eba0c2
chelo-py <74100371+chelo-py@users.noreply.github.com>
2021-02-01 17:01:23 -0300

    lineup

--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,53 @@
-const { series, src, dest, watch } = require('gulp');
+const { series, src, dest, watch, parallel } = require('gulp');
 const sass = require('gulp-sass');
+const imagemin = require('gulp-imagemin');
+const notify = require('gulp-notify');
+const webp = require('gulp-webp');
 
 // Función que compila SASS
 
+const paths = {
+    imagenes: 'src/img/**/*',
+    scss: 'src/scss/**/*.scss'
+}
+
 function css() {
-    return src('src/scss/app.scss')
-        .pipe(sass())
+    return src(paths.scss)
+        .pipe(sass({
+            outputStyle: 'expanded'
+        }))
         .pipe(dest('./build/css'))
 }
 
 function minificarcss() {
-    return src('src/scss/app.scss')
+    return src(paths.scss)
         .pipe(sass({
             outputStyle: 'compressed'
         }))
         .pipe(dest('./build/css'))
 }
 
+function imagenes() {
+    return src(paths.imagenes)
+        .pipe(imagemin())
+        .pipe(dest('./build/img'))
+        .pipe(notify({ message: 'Imagen Minificada' }))
+}
+
+function versionWebp() {
+    return src(paths.imagenes)
+        .pipe(webp())
+        .pipe(dest('./build/img/'))
+        .pipe(notify({ message: 'Versión Webp Lista' }))
+}
+
 function watchArchivos() {
-    watch('src/scss/**/*.scss', css);
+    watch(paths.scss, css);
 }
 
 exports.css = css;
 exports.minificarcss = minificarcss;
+exports.imagenes = imagenes;
 exports.watchArchivos = watchArchivos;
+
+exports.default = series(css, imagenes, versionWebp, watchArchivos);
